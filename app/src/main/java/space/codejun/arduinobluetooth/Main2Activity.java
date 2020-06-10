8ed644480ee976841e66c9a2f5cd1bad3b1a32c8
y0s30b <y0s30b.k@gmail.com>
2020-06-10 16:34:01 +0900

    옷 디스플레이 성공

--- app/src/main/java/space/codejun/arduinobluetooth/Main2Activity.java
+++ app/src/main/java/space/codejun/arduinobluetooth/Main2Activity.java
@@ -1,7 +1,10 @@
 package space.codejun.arduinobluetooth;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
@@ -15,5 +18,14 @@ public class Main2Activity extends AppCompatActivity {
 
         String tempText = ((MainActivity)MainActivity.context_main).ClothSectionVal;
         ClothSectionTV2.setText(tempText);
+
+        ImageView upIV = findViewById(R.id.up_image);
+        ImageView downIV = findViewById(R.id.down_image);
+
+        Button anotherRecommendBtn = findViewById(R.id.anotherRecommend);
+
+        Drawable draw = getResources().getDrawable(R.drawable.up_spring_1);
+        //Drawable draw = res.getDrawable( R.drawable.image_name_in_drawable );
+        upIV.setImageDrawable(draw); 
     }
 }
