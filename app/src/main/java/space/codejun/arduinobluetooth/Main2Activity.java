e3fdbf508593558ca69e0602419680b6ee3cfa50
y0s30b <y0s30b.k@gmail.com>
2020-06-10 16:05:31 +0900

    옷 추천 기능 레이아웃 완성

--- app/src/main/java/space/codejun/arduinobluetooth/Main2Activity.java
+++ app/src/main/java/space/codejun/arduinobluetooth/Main2Activity.java
@@ -2,6 +2,7 @@ package space.codejun.arduinobluetooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView ClothSectionTV2 = findViewById(R.id.ClothSection);
+        //ClothSectionTV2.setText("봄/가을 두꺼운 옷 구역");
     }
 }
